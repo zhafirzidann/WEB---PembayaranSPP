52c33077be6d7a81a1eebcb8e28e78236213b751
Zhafir Zidan <gerhana.total753@gmail.com>
2023-08-30 18:02:09 +0700

    First Commit

---
+++ application/views/navbar.php
@@ -0,0 +1,10 @@
+  <nav class="main-header navbar navbar-expand navbar-success navbar-light">
+    <!-- Left navbar links -->
+    <marquee direction="left" scrollamount="20" align="right"><font face="Arial Black" style="color:white">"PEMBAYARAN SPP PALING BOSOK SEPANJANG SEJARAH"
+    </font></marquee>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link">
+    </ul>
+  </nav>
+  <!-- /.navbar -->
